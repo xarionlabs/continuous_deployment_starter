d97ca92891b95ca93c7b8e3578a152faf30be85f
Erdi Calli <erdicalli@gmail.com>
2025-01-13 15:07:10 +0100

    adding another test project.

---
+++ applications/app_2/src/application.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+def get_str():
+    return "hello world this is our first application!!"
+
+
+st.text(get_str())
---
+++ applications/app_2/test/test_application.py
@@ -0,0 +1,4 @@
+from application import get_str
+
+def test_get_str():
+    assert get_str() == "hello world this is our first application!!"
