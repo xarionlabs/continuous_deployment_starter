b905c4798795206e397ec788b0aefe6cf4100675
Erdi Calli <erdicalli@gmail.com>
2025-01-13 15:08:07 +0100

    failing tests for the second application

--- applications/app_2/src/application.py
+++ applications/app_2/src/application.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 def get_str():
-    return "hello world this is our first application!!"
+    return "hello world this is our second application!!"
 
 
 st.text(get_str())
