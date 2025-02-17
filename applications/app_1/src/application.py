4141fe62c707812ddb007d317736d346ddb2edd2
Erdi Calli <erdicalli@gmail.com>
2025-02-17 13:40:25 +0100

    changed some code to see how fast builds take

--- applications/app_1/src/application.py
+++ applications/app_1/src/application.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 def get_str():
-    return "hello world this is our first application!!"
+    return "hello world this is our first application! welcome to my world."
 
 
 st.text(get_str())
