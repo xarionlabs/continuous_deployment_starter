309c8caa4d4ac46f40b725fb9c0026f937413aa1
Erdi Calli <erdicalli@gmail.com>
2025-03-17 09:24:55 +0100

    made lots of changes. let's see what will happen now.

--- utilities/user_management/entrypoint.sh
+++ utilities/user_management/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-#
+
 export PYTHONPATH=$(pwd)/src:$PYTHONPATH
 python -m manage_users
 export PGPASSWORD=$POSTGRES_PASSWORD
-psql -h db -U postgres -f /app/users.sql
+
+if [ ! -f /app/users.sql ]; then
+    echo "users.sql file not found!"
+    exit 1
+fi
+
+psql -h db -U $POSTGRES_USER -f /app/users.sql
