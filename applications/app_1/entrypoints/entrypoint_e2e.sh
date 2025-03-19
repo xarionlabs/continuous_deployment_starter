cb4ff29f0fc15aee56262dee02fd073ca3a5652e
Erdi Calli <erdicalli@gmail.com>
2025-03-19 20:30:55 +0100

    fixing entrypoints and e2e scripts. passing variables into docker run with a hacky solution

--- applications/app_1/entrypoints/entrypoint_e2e.sh
+++ applications/app_1/entrypoints/entrypoint_e2e.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Waiting for API to be ready..."
-wget --waitretry=2 --retry-connrefused --tries=10 -O /dev/null http://${APP_1_API_VIRTUAL_HOST}:8000/health || (echo 'API failed to start' && exit 1)
+wget --waitretry=2 --retry-connrefused --tries=10 -O /dev/null http://${APP_1_API_VIRTUAL_HOST}/health || (echo 'API failed to start' && exit 1)
 
 echo "Running e2e tests..."
-pytest e2e_tests/ -v --api-url=http://${APP_1_API_VIRTUAL_HOST}:8000 --api-key=${STAGING_API_KEY}
+pytest e2e_tests/ -v --api-url=http://${APP_1_API_VIRTUAL_HOST} --api-key=${APP_1_API_KEY}
